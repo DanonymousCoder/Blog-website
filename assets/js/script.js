3c269f6cb60f9f6d3698fdec4f5c5772d6ea349d
Ridwan <aladebaruwa@gmail.com>
2024-07-26 21:58:59 +0100

    Added slider functionality with next and previous buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,4 +54,45 @@ const sliderContainer = document.querySelector("[data-slider-container]");
 const sliderPrevBtn = document.querySelector("[data-slider-prev]");
 const sliderNextBtn = document.querySelector("[data-slider-next]");
 
-let totalSliderVisibleItems = Number(getComputedStyle(slider).getpropertyValue)
+let totalSliderVisibleItems = Number(getComputedStyle(slider).getpropertyValue("-slider-items"));
+let totalSlidableItems = sliderContainer.childElementCount - totalSliderVisibleItems;
+
+let currentSlide = 0;
+
+const moveSliderItem = function () {
+    sliderContainer.style.transform = `translateX(-${sliderContainer.children[currentSlidePos].offsetLeft}px)`;
+}
+
+/**
+ * NEXT SLIDE
+ */
+
+const slideNext = function () {
+    const slideEnd = currentSlidePos >= totalSlidableItems;
+
+    if (slideEnd) {
+        currentSlidePos = 0;
+    } else {
+        currentSlidePos++;
+    }
+
+    moveSliderItem();
+}
+
+sliderNextBtn.addEventListener("click", slideNext);
+
+/**
+ * PREVIOUS SLIDE
+ */
+
+const slidePrev = function () {
+    if (currentSlidePos <= 0) {
+        currentSlidePos = totalSlidableItems;
+    } else {
+        currentSlidePos--;
+    }
+
+    moveSliderItem();
+}
+
+sliderPrevBtn.addEventListener("click", slidePrev);
