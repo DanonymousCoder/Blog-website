e750d41b5df4ffdb73b918f8d31465e8b0140183
Ridwan <aladebaruwa@gmail.com>
2024-07-27 21:17:59 +0100

    Added back to top button with styles and functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,12 +26,15 @@ addEventOnElements(navTogglers, "click", toggleNav);
  * When scrolled down to 100px header will be active
  */
 const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
 
 window.addEventListener("scroll", () => {
     if (window.scrollY > 100) {
         header.classList.add("active");
+        backTopBtn.classList.add("active");
     } else {
         header.classList.remove("active");
+        backTopBtn.classList.remove("active");
     }
 });
 
