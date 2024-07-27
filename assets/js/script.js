29dce91b328e9f9b61a64bd437b5ad2880551a08
Ridwan <aladebaruwa@gmail.com>
2024-07-27 11:10:21 +0100

    "Updated CSS, JS, and HTML files: added new styles for navbar, modified event listeners and slider functionality, and added a new section for popular tags in the HTML.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,27 @@
 'use strict';
 
 /**
- *  Add event listener on multiple elements
+ * Add event listener on multiple elements
  */
-
 const addEventOnElements = function(elements, eventType, callback) {
     for (let i = 0, len = elements.length; i < len; i++) {
         elements[i].addEventListener(eventType, callback);
     }
 }
 
-
-
-
-
 /**
  * MOBILE NAVBAR TOGGLER
  */
-
 const navBar = document.querySelector("[data-navbar]");
 const navTogglers = document.querySelectorAll("[data-nav-toggler]");
 const toggleNav = () => navBar.classList.toggle("active");
 
 addEventOnElements(navTogglers, "click", toggleNav);
 
-
-
-
 /**
  * HEADER ANIMATION
  * When scrolled down to 100px header will be active
  */
-
 const header = document.querySelector("[data-header]");
 
 window.addEventListener("scroll", () => {
@@ -42,38 +32,33 @@ window.addEventListener("scroll", () => {
     }
 });
 
-
-
-
 /**
  * SLIDER
  */
-
 const slider = document.querySelector("[data-slider]");
 const sliderContainer = document.querySelector("[data-slider-container]");
 const sliderPrevBtn = document.querySelector("[data-slider-prev]");
 const sliderNextBtn = document.querySelector("[data-slider-next]");
 
-let totalSliderVisibleItems = Number(getComputedStyle(slider).getpropertyValue("-slider-items"));
+let totalSliderVisibleItems = Number(getComputedStyle(slider).getPropertyValue("--slider-items"));
 let totalSlidableItems = sliderContainer.childElementCount - totalSliderVisibleItems;
 
 let currentSlide = 0;
 
 const moveSliderItem = function () {
-    sliderContainer.style.transform = `translateX(-${sliderContainer.children[currentSlidePos].offsetLeft}px)`;
+    sliderContainer.style.transform = `translateX(-${sliderContainer.children[currentSlide].offsetLeft}px)`;
 }
 
 /**
  * NEXT SLIDE
  */
-
 const slideNext = function () {
-    const slideEnd = currentSlidePos >= totalSlidableItems;
+    const slideEnd = currentSlide >= totalSlidableItems;
 
     if (slideEnd) {
-        currentSlidePos = 0;
+        currentSlide = 0;
     } else {
-        currentSlidePos++;
+        currentSlide++;
     }
 
     moveSliderItem();
@@ -84,12 +69,11 @@ sliderNextBtn.addEventListener("click", slideNext);
 /**
  * PREVIOUS SLIDE
  */
-
 const slidePrev = function () {
-    if (currentSlidePos <= 0) {
-        currentSlidePos = totalSlidableItems;
+    if (currentSlide <= 0) {
+        currentSlide = totalSlidableItems;
     } else {
-        currentSlidePos--;
+        currentSlide--;
     }
 
     moveSliderItem();
@@ -101,8 +85,8 @@ sliderPrevBtn.addEventListener("click", slidePrev);
  * RESPONSIVE
  */
 window.addEventListener("resize", function () {
-    totalSliderVisibleItems = Number(getComputedStyle(slider).getpropertyValue("-slider-items"));
+    totalSliderVisibleItems = Number(getComputedStyle(slider).getPropertyValue("--slider-items"));
     totalSlidableItems = sliderContainer.childElementCount - totalSliderVisibleItems;
 
     moveSliderItem();
-})
+});
